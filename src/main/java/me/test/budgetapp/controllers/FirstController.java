8213c203c3dd8a620cf3bf366bc9cf352eb04e39
Tomokou <bychckova.margo@yandex.ru>
2023-03-11 22:41:59 +0300

    Initial commit

--- src/main/java/me/test/budgetapp/controllers/FirstController.java
+++ src/main/java/me/test/budgetapp/controllers/FirstController.java
@@ -1,8 +1,13 @@
 package me.test.budgetapp.controllers;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "Первый контроллер", description = "Краткое ИНФО о приложении")
 public class FirstController {
 
     @GetMapping
@@ -10,8 +15,11 @@ public class FirstController {
         return "Приложение запущено";
     }
 
-
-
+    @Operation(summary = "ИНФО о приложении", description = "Краткая сводка информации об исполнителе проекта, названии, дате создания и краткого описания")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Запрос выполнен")
+    }
+    )
     @GetMapping("/info")
     public String info() {
         return "Проект выполняет Бычкова Маргарита. " +
--- src/main/java/me/test/budgetapp/service/Ingredient.java
+++ src/main/java/me/test/budgetapp/service/Ingredient.java
@@ -1,40 +1,11 @@
 package me.test.budgetapp.service;
 
+import lombok.Data;
+
+@Data
 public class Ingredient {
     private final String ingredientName;
-
     private final Integer ingredientQuantity;
-
     private final String measureUnit;
 
-    public Ingredient( String ingredientName, Integer ingredientQuantity, String measureUnit) {
-        if (ingredientName.isBlank() || ingredientName.isEmpty()) {
-            this.ingredientName = "Название ингредиента не указано";
-        } else {
-            this.ingredientName = ingredientName;
-        }
-        if (ingredientQuantity == null || ingredientQuantity <=0) {
-            this.ingredientQuantity = 1;
-        } else {
-            this.ingredientQuantity = ingredientQuantity;
-        }
-        if (measureUnit.isEmpty() || measureUnit.isBlank()) {
-            this.measureUnit = "Единица измерения не указана";
-        } else {
-            this.measureUnit = measureUnit;
-        }
-    }
-
-
-    public String getIngredientName() {
-        return ingredientName;
-    }
-
-    public Integer getIngredientQuantity() {
-        return ingredientQuantity;
-    }
-
-    public String getMeasureUnit() {
-        return measureUnit;
-    }
 }
